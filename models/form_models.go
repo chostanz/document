5a09e35c52752471efa7766b0cd648da4123e9f9
chostanz <dewianyar21@gmail.com>
2024-02-05 09:09:34 +0700

    P

--- models/form_models.go
+++ models/form_models.go
@@ -6,18 +6,18 @@ import (
 )
 
 type Form struct {
-	UUID          string         `json:"form_uuid" db:"form_uuid"`
-	Document_UUID string         `json:"document_uuid" db:"document_uuid" validate:"required"`
-	User_id       string         `json:"user_id" db:"user_id" validate:"required"`
-	Form_number   string         `json:"form_number" db:"form_number" validate:"required"`
-	Form_ticket   string         `json:"form_ticket" db:"form_ticket" validate:"required"`
-	Form_status   string         `json:"form_status" db:"form_status"`
-	Created_by    string         `json:"created_by" db:"created_by"`
-	Created_at    time.Time      `json:"created_at" db:"created_at"`
-	Updated_by    sql.NullString `json:"updated_by" db:"updated_by"`
-	Updated_at    sql.NullTime   `json:"updated_at" db:"updated_at"`
-	Deleted_by    sql.NullString `json:"deleted_by" db:"deleted_by"`
-	Deleted_at    sql.NullTime   `json:"deleted_at" db:"deleted_at"`
+	UUID         string         `json:"form_uuid" db:"form_uuid"`
+	DocumentUUID string         `json:"document_uuid" db:"document_uuid" validate:"required"`
+	UserID       string         `json:"user_id" db:"user_id" validate:"required"`
+	FormNumber   string         `json:"form_number" db:"form_number" validate:"required"`
+	FormTicket   string         `json:"form_ticket" db:"form_ticket" validate:"required"`
+	FormStatus   string         `json:"form_status" db:"form_status"`
+	Created_by   string         `json:"created_by" db:"created_by"`
+	Created_at   time.Time      `json:"created_at" db:"created_at"`
+	Updated_by   sql.NullString `json:"updated_by" db:"updated_by"`
+	Updated_at   sql.NullTime   `json:"updated_at" db:"updated_at"`
+	Deleted_by   sql.NullString `json:"deleted_by" db:"deleted_by"`
+	Deleted_at   sql.NullTime   `json:"deleted_at" db:"deleted_at"`
 }
 
 type Forms struct {
